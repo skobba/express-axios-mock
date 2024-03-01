1cc0ed7df39cc9f11becb51f5382658dddec8a76
Gjermund Skobba <gjermund@skobba.net>
2024-03-01 10:17:20 +0100

    Add api request

---
+++ api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const axiosInstance = axios.create();
+
+export const fetchData = async () => {
+  const res = await axiosInstance.get(apiUrl);
+  return res.data;
+};
+
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import {fetchData} from './api';
 
 //For env File 
 dotenv.config();
@@ -7,8 +8,9 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+app.get('/', async (req: Request, res: Response) => {
+  const data = await fetchData();
+  res.send(data);
 });
 
 app.listen(port, () => {
