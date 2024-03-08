d24884a88d0eeab62897232d742ad7c8a7435db6
Gjermund Skobba <gjermund@skobba.net>
2024-03-08 18:14:05 +0100

    Add interceptoer

--- proxy/api/api.ts
+++ proxy/api/api.ts
@@ -3,6 +3,16 @@ const apiUrl = 'http://localhost:8001';
 
 const axiosInstance = createAxiosInstance();
 
+// Add a request interceptor
+const requestInterceptor = axiosInstance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  console.log('Intercepted GET request:', config.url);
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
 export const fetchData = async () => {
   const res = await axiosInstance.get(apiUrl);
   return res;
