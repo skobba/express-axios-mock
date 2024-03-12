d768f6e505b474deb74b2925c90cd6e19f798c99
Gjermund Skobba <gjermund@skobba.net>
2024-03-12 14:44:25 +0100

    Update jest.config.js with usefull parameters.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,49 @@
+const TEST = process.env.TEST || 'unit';
+const SCOPE = process.env.SCOPE || 'all';
+process.env.TZ = 'Europe/Oslo';
+
 module.exports = {
-    "roots": [
-      "<rootDir>/"
-    ],
-    "testMatch": [
+    displayName: 'tests',
+    testEnvironment: 'node',
+    resetMocks: true,
+    setupFiles: [],
+    setupFilesAfterEnv: [],
+    moduleNameMapper: {},
+    coverageThreshold: {
+      global: {
+        branches: 70,
+        functions: 50,
+        lines: 75,
+        statements: 50
+      }
+    },
+    globals: {
+      __DEV__: true,
+      'window': {}
+    },
+    roots: ["<rootDir>/"],
+    testMatch: [
       "**/__tests__/**/*.+(ts|tsx|js)",
       "**/?(*.)+(spec|test).+(ts|tsx|js)"
     ],
-    "transform": {
+    transform: {
       "^.+\\.(ts|tsx)$": "ts-jest"
     },
-    "moduleDirectories": ["node_modules", "__mocks__"],
-    "moduleNameMapper": {}
+    moduleDirectories: ["node_modules", "__mocks__"],
+    moduleNameMapper: {},
+    moduleFileExtensions: [
+      'js',
+      'jsx',
+      'ts',
+      'tsx',
+      'json'
+    ],
+    testEnvironmentOptions: {
+      url: 'http://localhost',
+      resultsDir: './allure-results'
+    },
+    modulePaths: [
+      'src'
+    ],
+    testTimeout: 40000,
   }
